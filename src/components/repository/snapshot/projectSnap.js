11692550fdcdcfb6c4c317cb35abbbbbedb5e83d
Sean Harte <scsharte@gmail.com>
2018-05-15 23:45:55 -0400

    added repository/snapshot components

--- src/components/repository/snapshot/projectSnap.js
+++ src/components/repository/snapshot/projectSnap.js
@@ -3,7 +3,7 @@ import './projectSnap.css';
 
 export default function SnapShot(props) {
 	return (
-		<li className="projectSnapShot">
+		<li className="projectSnapShot" key={props.index}>
 	    	<h3 className="projectSnapShotTitle">{props.projectName}</h3>
 	        <p className="projectSnapShotDec">{props.projectDec}</p>
 	        <div className="projectSnapShotRole">{props.userRole}</div>
