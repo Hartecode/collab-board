216100a6fcb81444d567e4b97e9d25c193a26e42
Sean Harte <scsharte@gmail.com>
2018-05-18 22:58:41 -0400

    added pages links to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './header.css';
 
@@ -7,16 +8,18 @@ export default function Header() {
 		<header role="banner">
 	      <nav role="navagation">
 	        <ul className="navLinkContainer">
-	          <li><a href="#">CB</a></li>
-	          <li>
-	            <a href="#"><i className="fas fa-users"></i></a>
-	          </li>
-	          <li>
-	            <a href="#"><i className="fas fa-clipboard-list"></i></a>
-	          </li>
-	          <li>
-	            <a href="#"><img className="userImage" src="https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg" alt="User name" /></a>
-	          </li>
+	        	<li>
+	        		<Link to='/dashboard'>CB</Link>
+	        	</li>
+	        	<li>
+	        		<Link to="/request"><i className="fas fa-users"></i></Link>
+	        	</li>
+	        	<li>
+	        		<Link to="/board"><i className="fas fa-clipboard-list"></i></Link>
+	       		</li>
+	        	<li>
+	        		<a href="#"><img className="userImage" src="https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg" alt="User name" /></a>
+	        	</li>
 	        </ul>
 	      </nav>
 	    </header>
