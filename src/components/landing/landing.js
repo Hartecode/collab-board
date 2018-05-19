a4a6e3685f62bb9fbfeb0404afa4c77fdeb0e4ec
Sean Harte <scsharte@gmail.com>
2018-05-18 22:30:55 -0400

    updated landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -3,10 +3,10 @@ import './landing.css';
 
 export default function LandingPage() {
 	return (
-		<div>
+		<div className="LandingContainer">
 			<h1>Collab Board</h1>
 	    	<p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras luctus sit amet tellus iaculis auctor. Curabitur sed leo mi. Etiam rutrum tincidunt massa sit amet lobortis. Phasellus venenatis aliquet arcu id pulvinar." </p>
-	    	<button>Lets Collab</button>
+	    	<button className="startButton">Lets Collab</button>
 		</div>
 	);
 }
