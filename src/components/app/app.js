6d2e6222257261ef85a57e138189e376b48e97be
Sean Harte <scsharte@gmail.com>
2018-05-18 22:33:02 -0400

    added react routers

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import Header from '../header/header';
-// import Dashboard from '../dashboard/dashboard';
-// import Board from '../board/board';
-// import Project from '../repository/project';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import LandingPage from '../landing/landing';
+import Header from '../header/header';
+import Dashboard from '../dashboard/dashboard';
+import Board from '../board/board';
+import Project from '../repository/project';
+import Request from '../request/request';
+
+
 
 import './app.css';
 
 export default function App() {
 
 	return (
-		<div>
-			<LandingPage />
-
-		</div>
+		<Router>
+			<div>
+				<Route path="/" render={(props) => (props.location.pathname !== "/" ) && <Header /> }/>
+				<Route exact path="/" component={LandingPage} />
+				<Route exact path="/dashboard" component={Dashboard} />
+				<Route exact path="/board" component={Board} />
+				<Route exact path="/project/:projectId" component={Project} />
+				<Route exact path="/request" component={Request} />
+			</div>
+		</Router>
 	);
 }
