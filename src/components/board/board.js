39f3f2a1e50c88b4b891b829ce8c2a3db54454ca
Sean Harte <scsharte@gmail.com>
2018-06-02 11:34:08 -0400

    add redux to the board

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,213 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import SnapShot from '../repository/snapshot/projectSnap';
 import './board.css';
 
-export default class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			user : {
-				id : 64532,
-				name: 'User Name',
-				avatarImgUrl: "https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg",
-				githubLink: "www.github.com"
-			},
-			repositories : [
-				{
-					id: 23434,
-					projectName: 'Project Name',
-					projectDec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim justo eget aliquet efficitur. Curabitur finibus pellentesque fermentum. Aenean urna magna, maximus sed justo ac, ultrices laoreet odio. Curabitur hendrerit sollicitudin leo id convallis.',
-					projectLink: 'www.github.com/name/project',
-					ownerID: 66666,
-					ownerAvatarUrl: "https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg",
-					collaborators: [],
-					pendingRequest: [
-						{
-							userID: 888888
-						}
-					]
-					
-				},
-				{
-					id: 4447,
-					projectName: 'Project Name',
-					projectDec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim justo eget aliquet efficitur. Curabitur finibus pellentesque fermentum. Aenean urna magna, maximus sed justo ac, ultrices laoreet odio. Curabitur hendrerit sollicitudin leo id convallis.',
-					projectLink: 'www.github.com/name/project',
-					ownerID: 66666,
-					ownerAvatarUrl: "https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg",
-					collaborators: [
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						}
-					],
-					pendingRequest: [
-						{
-							userID: 888888
-						}
-					]
-					
-				},
-				{
-					id: 58976,
-					projectName: 'Project Name',
-					projectDec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim justo eget aliquet efficitur. Curabitur finibus pellentesque fermentum. Aenean urna magna, maximus sed justo ac, ultrices laoreet odio. Curabitur hendrerit sollicitudin leo id convallis.',
-					projectLink: 'www.github.com/name/project',
-					ownerID: 64532,
-					ownerAvatarUrl: "https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg",
-					collaborators: [
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						}
-					],
-					pendingRequest: [
-						{
-							userID: 888888
-						}
-					]
-					
-				},
-				{
-					id: 89898,
-					projectName: 'Project Name',
-					projectDec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim justo eget aliquet efficitur. Curabitur finibus pellentesque fermentum. Aenean urna magna, maximus sed justo ac, ultrices laoreet odio. Curabitur hendrerit sollicitudin leo id convallis.',
-					projectLink: 'www.github.com/name/project',
-					ownerID: 66666,
-					ownerAvatarUrl: "https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg",
-					collaborators: [
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						}
-					],
-					pendingRequest: [
-						{
-							userID: 64532
-						}
-					]
-					
-				},
-				{
-					id: 99999,
-					projectName: 'Project Name',
-					projectDec: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim justo eget aliquet efficitur. Curabitur finibus pellentesque fermentum. Aenean urna magna, maximus sed justo ac, ultrices laoreet odio. Curabitur hendrerit sollicitudin leo id convallis.',
-					projectLink: 'www.github.com/name/project',
-					ownerID: 66666,
-					ownerAvatarUrl: "https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg",
-					collaborators: [
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"http://www.todayifoundout.com/wp-content/uploads/2017/12/myspace-tom.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"http://www.todayifoundout.com/wp-content/uploads/2017/12/myspace-tom.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"http://www.todayifoundout.com/wp-content/uploads/2017/12/myspace-tom.jpg"
-						},
-						{
-							userID: 64532,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						},
-						{
-							userID: 5555,
-							avatarImgUrl:"https://uploads.teamtreehouse.com/production/profile-photos/1179602/thumb_profile.jpg"
-						}
-					],
-					pendingRequest: [
-						{
-							userID: 888888
-						}
-					]
-					
-				}
+export function Board(props) {
 
-			]
-		}
+	const boardList = () => {
+		const repository = props.boardRepo;
+
+		return repository.map((file, index) => {
+			let totalOfCollabs = file.collaborators.length;
+			let mainUserId = props.mainUser.id;
+			let pending = (1 === file.pendingRequest.filter(req => req.userID === mainUserId).length) ? 'Pending': <button className="projectSnapShotJoin">Join Team</button>;
+
+			return <SnapShot 
+		        		
+		        		id={file.id}
+		        		key={file.id}
+		        		projectName={file.projectName}
+		        		projectDec={file.projectDec}
+		 				userRole={pending}
+		        		numberOfCollabs={totalOfCollabs}
+		        	/>
+		});
 	}
 
-
-	render() {
-		const boardList = () => {
-			const repository = this.state.repositories;
-
-			return repository.map((file, index) => {
-				let totalOfCollabs = file.collaborators.length;
-				let mainUserId = this.state.user.id;
-				let pending = (1 === file.pendingRequest.filter(req => req.userID === mainUserId).length) ? 'Pending': <button className="projectSnapShotJoin">Join Team</button>;
-
-				return <SnapShot 
-			        		
-			        		id={file.id}
-			        		key={file.id}
-			        		projectName={file.projectName}
-			        		projectDec={file.projectDec}
-			 				userRole={pending}
-			        		numberOfCollabs={totalOfCollabs}
-			        	/>
-			});
-		}
-
-		return (
-			<main role="main">
-		    	<section>
-		        	<h2 className="pageTilte">Board</h2>
-		        	<ul className="userprojectlist" aria-live="assertive">
-			        	{boardList()}
-		        	</ul>
-		    	</section>
-		    </main>
-		);
-	}
+	return (
+		<main role="main">
+	    	<section>
+	        	<h2 className="pageTilte">
+	        		Board
+	        	</h2>
+	        	<ul className="userprojectlist" aria-live="assertive">
+		        	{boardList()}
+	        	</ul>
+	    	</section>
+	    </main>
+	);
 }
+
+const mapStateToProps = state => ({
+    boardRepo: state.boardRepo,
+    mainUser: state.mainUser
+});
+
+export default connect(mapStateToProps)(Board);
