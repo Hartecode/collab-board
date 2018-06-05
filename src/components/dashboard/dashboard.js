9ba2f56cffea54838686f574909765108e0111e4
Sean Harte <scsharte@gmail.com>
2018-06-05 00:36:39 -0400

    added submited form for new projects

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -5,47 +5,47 @@ import Profile from './profile';
 import './dashboard.css';
 import { fetchLoginUser } from '../../actions';
 
-export function Dashboard(props) {
-
- 	props.dispatch(fetchLoginUser());
-
-	const displayUserRepo = () => {
-		const userProjects = props.userRepo;
-		if(userProjects.length > 0) {
-			return userProjects.map( project => {
-				let totalOfCollabs = project.collaborators.length;
-				let userRole = (project.ownerID === props.mainUser.id)? 'Owner' : 'Co-Collab';
-				console.log(props.mainUser.id);
-				return <SnapShot 
-			        		id={project.id}
-			        		key={project.id}
-			        		transferProject={project}
-			        		projectName={project.projectName}
-			        		projectDec={project.projectDec}
-			        		userRole={userRole}
-			        		numberOfCollabs={totalOfCollabs}
-			        	/>
-			})
-		} else {
-			return <li><h2> There are no projects posted.</h2></li>
-		}
+export class Dashboard extends React.Component {
+	
+	componentDidMount() {
+		this.props.dispatch(fetchLoginUser());
 	}
+ 	
+ 	render() {
+ 		const displayUserRepo = () => {
+			const userProjects = this.props.userRepo;
+			if(userProjects.length > 0) {
+				return userProjects.map( project => {
+					let totalOfCollabs = project.collaborators.length;
+					let userRole = (project.ownerID === this.props.mainUser.id)? 'Owner' : 'Co-Collab';
+					return <SnapShot 
+				        		id={project.id}
+				        		key={project.id}
+				        		transferProject={project}
+				        		projectName={project.projectName}
+				        		projectDec={project.projectDec}
+				        		userRole={userRole}
+				        		numberOfCollabs={totalOfCollabs}
+				        	/>
+				})
+			} else {
+				return <li><h2> There are no projects posted.</h2></li>
+			}
+		}
 
-	return (
-		<main className="dashboardContainer" role="main">
-	      <Profile 
-	      	avatarImgUrl={props.mainUser.avatarUrl}
-	      	userName= {props.mainUser.username}
-	      	githubLink= {props.mainUser.githubProfileUrl}
-	      />
-	      <section>
-	        <h2 className="repoTitle">Repository</h2>
-	        <ul className="userprojectlist" aria-live="assertive">
-	        	{displayUserRepo()}
-	        </ul>
-	      </section>
-	    </main>
-	);
+		return (
+			<main className="dashboardContainer" role="main">
+		      <Profile />
+		      <section role="region">
+		        <h2 className="repoTitle">Repository</h2>
+		        <ul className="userprojectlist" aria-live="assertive">
+		        	{displayUserRepo()}
+		        </ul>
+		      </section>
+		    </main>
+		);
+ 	}
+	
 	
 }
 
--- src/components/dashboard/profile.js
+++ src/components/dashboard/profile.js
@@ -1,17 +1,112 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './profile.css';
 
-export default function Profile(props) {
-	return (
-		<section className="profile" role="section">
-			<div className="profileElements">
-		    	<img className="profileImage" src={props.avatarImgUrl} alt={props.userName} />
+export class Profile extends React.Component  {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	    	addProject: false,
+	    	githubRepo: null,
+	    	selectValue: ''
+	    };
+	    this.onClickPro = this.onClickPro.bind(this);
+	    this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	componentDidMount() {
+		fetch("https://api.github.com/users/Hartecode/repos")
+	        .then(res => {
+	            if (!res.ok) {
+	                return Promise.reject(res.statusText);
+	            }
+	            console.log(res);
+	            return res.json();
+	        })
+	        .then( repo => {
+	        	this.setState({
+	        		githubRepo: repo
+	        	});
+	        });
+	}
+
+
+	onClickPro() {
+		this.setState( prevState => ({
+        	addProject: !prevState.addProject
+        }));
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		console.log(`submited ${e.target.value} `)
+	}
+
+	onChange(e) {
+		this.setState({
+			selectValue:e.target.value
+		})
+	}
+
+	render() {
+
+		const listOfRepo = () => {
+			const repos = this.state.githubRepo;
+			if(this.state.githubRepo) {
+				return repos.map( pro => {
+					return <option key={pro.id} value={pro.name}>{pro.name}</option>
+				});
+			}
+		}
+
+		return (
+			<div>
+				<section className="profile" role="section">
+					<div className="profileElements">
+				    	<img className="profileImage" 
+				    		src={this.props.mainUser.avatarUrl} 
+				    		alt={this.props.mainUser.userName} 
+				    	/>
+				    </div>
+				    <div className="profileElements">
+				    	<p>
+				    		{this.props.mainUser.userName}
+				    	</p>
+				    	<p>
+				    		<a href={this.props.mainUser.githubLink}>
+				    			Github Profile
+				    		</a>
+				    	</p>
+				    	<a  onClick={this.onClickPro}>
+				    		<i className="fas fa-plus-circle"></i> 
+				    		Add Project
+				    	</a>
+				    </div>
+				</section>
+
+				<section className={(this.state.addProject)? '': 'hide'} role="region">
+			      	<form onSubmit={this.onSubmit}>
+			      		<fieldset>
+			      			<legend>
+			      				Select a project to add
+			      			</legend>
+
+			      			<select name="repo" value={this.state.selectValue} onChange={this.onChange}>
+							    {listOfRepo()}
+							</select>
+							<br /><br />
+							<input type="submit" />
+				      		
+						</fieldset>
+			      	</form>
+			    </section>
 		    </div>
-		    <div className="profileElements">
-		    	<p>{props.userName}</p>
-		    	<p><a href="#">{props.githubLink}</a></p>
-		    	<a href="#"><i className="fas fa-plus-circle"></i> Add Project</a>
-		    </div>
-		</section>
-	);
+		);
+	}
 }
+
+const mapStateToProps = state => ({
+    mainUser: state.mainUser,
+});
+
+export default connect(mapStateToProps)(Profile);
