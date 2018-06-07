eef9919674776fc6ae9219307a1200116a034b2b
Sean Harte <scsharte@gmail.com>
2018-06-06 23:31:48 -0400

    updated request compoent

--- src/components/request/request.js
+++ src/components/request/request.js
@@ -2,39 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Notice from './notification/notice';
 import './request.css';
+import { fetchRequests } from '../../actions';
 
-export function Request(props) {
 
-	const requestlist = props.requests;
+export class Request extends React.Component {
 
-	const list = () => {
-		if(requestlist.length > 0) {
-			return requestlist.map( (obj, index) => 
-				<Notice 
-					index={index}
-					key={obj.id}
-					projectName={obj.projectName}
-					avatarImgUrl={obj.avatarImgUrl}
-					requestDec={obj.requestDec}
-				/>
-			);
-		} else {
-			return <li><h3>No Requests</h3></li>
-		}
+	componentDidMount() {
+
 	}
-		
 
+	render() {
+		const requestlist = this.props.requests;
 
-	return (
-		<main role="main">
-	      <section>
-	        <h2 className="pageTilte">Collabs Requests</h2>
-	        <ul className="projectRequests" aria-live="assertive">
-	        	{list()}
-	        </ul>
-	      </section>
-	    </main>
-	);
+		const list = () => {
+			if(requestlist.length > 0) {
+				return requestlist.map( (obj, index) => 
+					<Notice 
+						index={index}
+						key={obj.id}
+						projectName={obj.projectName}
+						avatarImgUrl={obj.avatarImgUrl}
+						requestDec={obj.requestDec}
+					/>
+				);
+			} else {
+				return <li><h3>No Requests</h3></li>
+			}
+		}
+			
+		return (
+			<main role="main">
+		      <section>
+		        <h2 className="pageTilte">Collabs Requests</h2>
+		        <ul className="projectRequests" aria-live="assertive">
+		        	{list()}
+		        </ul>
+		      </section>
+		    </main>
+		);
+	}
+	
 
 }
 
