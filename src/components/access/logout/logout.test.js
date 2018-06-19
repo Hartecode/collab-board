6df37ac4bc28ba4444857b6ad4f4728763c95c2b
Sean Harte <scsharte@gmail.com>
2018-06-19 19:26:49 -0400

    updated logout test

--- src/components/access/logout/logout.test.js
+++ src/components/access/logout/logout.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
 import LogOut from './logout';
 
+const mockStore = configureMockStore();
+const store = mockStore({}); 
+
 describe('<LogOut />', () => {
-  it('Renders without crashing', () => {
-    shallow(<LogOut />);
-  });
+	it('Renders without crashing', () => {
+	    shallow(<Provider store={store}>
+	                <LogOut />
+	            </Provider>);
+	});
 });
