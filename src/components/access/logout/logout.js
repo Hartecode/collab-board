036fa32942f675623a8f52dc7ff0b1b8f1de6d6d
Sean Harte <scsharte@gmail.com>
2018-06-19 19:20:57 -0400

    connect logout to remote server

--- src/components/access/logout/logout.js
+++ src/components/access/logout/logout.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 import './logout.css';
 
-export default function LogOut() {
+export function LogOut(props) {
 	return (
 		<main className="" role='main'>
 			<div className="logoutBox">
 				<h1>Do you want to log out?</h1>
-				<a href="http://localhost:8080/auth/logout">
+				<a href="https://boiling-earth-62792.herokuapp.com/logout">
 					<button className="btn logoutBtn">
 						Yes
 					</button>
 				</a>
-				<Link to='/dashboard'>
+				<Link to={'/dashboard/' + props.mainUser.id}>
 					<button className="btn logoutBtn">
 						No, back to my dashboard
 					</button>
@@ -21,3 +22,9 @@ export default function LogOut() {
 		</main>
 	);
 }
+
+const mapStateToProps = state => ({
+    mainUser: state.mainUser,
+});
+
+export default connect(mapStateToProps)(LogOut);
